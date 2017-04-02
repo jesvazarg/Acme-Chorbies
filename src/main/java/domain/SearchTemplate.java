025d049333009c9ace5f5873233f640fb185f16d
sanframar <raznnok@gmail.com>
2017-04-02 12:59:50 +0200

    Populate y dominio de searchTemplate y Admin

--- src/main/java/domain/SearchTemplate.java
+++ src/main/java/domain/SearchTemplate.java
@@ -1,9 +1,21 @@
 
 package domain;
 
+import java.util.Collection;
+import java.util.Date;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -14,8 +26,83 @@ public class SearchTemplate extends DomainEntity {
 		super();
 	}
 
+
 	// Attributes -------------------------------------------------------------
+	private String		relationship;
+	private Integer		age;
+	private String		genre;
+	private Coordinate	coordinates;
+	private String		keyword;
+	private Date		updateMoment;
+
+
+	public String getRelationship() {
+		return this.relationship;
+	}
+
+	public void setRelationship(final String relationship) {
+		this.relationship = relationship;
+	}
+
+	@Min(0)
+	public Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(final Integer age) {
+		this.age = age;
+	}
+
+	public String getGenre() {
+		return this.genre;
+	}
+
+	public void setGenre(final String genre) {
+		this.genre = genre;
+	}
+
+	public Coordinate getCoordinates() {
+		return this.coordinates;
+	}
+
+	public void setCoordinates(final Coordinate coordinates) {
+		this.coordinates = coordinates;
+	}
+
+	public String getKeyword() {
+		return this.keyword;
+	}
+
+	public void setKeyword(final String keyword) {
+		this.keyword = keyword;
+	}
+
+	@Past
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	public Date getUpdateMoment() {
+		return this.updateMoment;
+	}
+
+	public void setUpdateMoment(final Date updateMoment) {
+		this.updateMoment = updateMoment;
+	}
+
 
 	// Relationships ----------------------------------------------------------
+	private Collection<Chorbi>	results;
+
+
+	@Valid
+	@NotNull
+	@ManyToMany()
+	public Collection<Chorbi> getResults() {
+		return this.results;
+	}
+
+	public void setResults(final Collection<Chorbi> results) {
+		this.results = results;
+	}
 
 }
